2177debdff42d9db86248f3642069907639a86e2
abiram-squashapps <abiram@squashapps.com>
2021-08-27 09:28:29 +0530

    netlify deployment

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,6 +1,5 @@
 import { createStore } from "redux";
-import { rootReducer } from "./Reducers";
+
 import globalFilterReducer from "./Reducers/globalFilter";
-import reducer from "./reduer";
 
 export const store = createStore(globalFilterReducer);
